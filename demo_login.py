00327561f78f009d2936a572045b61aff1ec28aa
adityadenny <aditaydenny6@gmail.com>
2022-03-13 17:59:21 +0700

    fix code from selenium3 to selenium4 format

--- demo_login.py
+++ demo_login.py
@@ -9,6 +9,6 @@ driver.maximize_window()
 
 driver.get("https://demoqa.com/login")
 driver.find_element(By.ID, "userName").send_keys("umarAlfatih")
+driver.find_element(By.ID, "password").send_keys("asDF12#$")
+driver.find_element(By.ID, "login").click()
 
-driver.find_element_by_id("password").send_keys("asDF12#$")
-driver.find_element_by_id("login").click()
